6025f423083093bae990a8f6a16d168e5c297430
GBleeBloop <53452244+GBleeBloop@users.noreply.github.com>
2021-12-13 11:28:00 +0000

    Updated cure cycle requirements

--- pages/cureCycleReqs.py
+++ pages/cureCycleReqs.py
@@ -1,7 +1,7 @@
 # this code needs to correspond with the constraints outlined in
 # section 6.1 of 73-011-ATP-(01)-0A-Base_Panel_Cure_Profile
-B_A = 110 * 60  # minimum cure time
-D_C = 150 * 60  # minimum post cure time
+B_A = 120 * 60  # minimum cure time
+D_C = 180 * 60  # minimum post cure time
 A = 120 * 60  # minimum initial cure time
 C = 3800 * 60  # maximum time between cure and post cure
 Emin, Emax = 65, 75  # centigrade
